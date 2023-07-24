d3b35162bbb2dfd494982a326adc373252fd3deb
unknown <katdemey@outlook.com>
2023-07-25 00:19:39 +0100

    hero section laid out w tailwind. TODO: Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header bg-yellow-300">
-        <div>
-          <p>Quacky Coders</p>
-          <h1>Revolutionize your debugging with monthly rubber ducks!</h1>
+      <header className="App-header bg-primary h-96 flex items-center justify-center text-left">
+        <div className="w-96">
+          <p className="font-bold align-text-top my-6">Quacky Coders</p>
+          <h1 className="text-4xl my-6 ">
+            Revolutionize your debugging with monthly rubber ducks!
+          </h1>
 
-          <div className="action-btns">
-            <button>Get Started</button>
-            <button>Learn More</button>
+          <div className="action-btns btn flex gap-2">
+            <button className="btn text-darkBlue bg-white">Get Started</button>
+            <button className="btn bg-lightBlue text-white">Learn More</button>
           </div>
         </div>
         <img
-          width={600}
+          className="h-48 rounded-sm drop-shadow-lg"
           src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fd2%2F16%2Feb%2Fd216eba9ad0abca8ecd567e2ed49f9a4.jpg&f=1&nofb=1&ipt=47acee959a7b38139fff346ad31cf7d609e668b5dd861a4ebdd07d528fa3cb2d&ipo=images"
           alt="rubber duck decoding"
         />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: '"Exo 2"',
+      },
+      container: {
+        padding: {
+          DEFAULT: "15px",
+        },
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1440px",
+      },
+      colors: {
+        primary: "#FFB703",
+        darkBlue: "#023047",
+        lightBlue: "#219EBC",
+        orange: "#FB8500",
+        greyFill: "#F2F2F2",
+      },
+      extend: {
+        backgroundImage: {
+          mainSlider: "url('img/mainSlider_bg.png')",
+        },
+        keyframes: {
+          shake: {
+            "10%, 90%": { transform: "translate3d(-1px, 0,0 )" },
+            "20%, 80%": { transform: "translate3d(2px, 0,0 )" },
+            "30%, 50%, 70%": { transform: "translate3d(-4px, 0,0 )" },
+            "40%, 60%": { transform: "translate3d(4px, 0,0 )" },
+          },
+        },
+        animation: {
+          shake: "shake 1s ease-in-out",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
 
